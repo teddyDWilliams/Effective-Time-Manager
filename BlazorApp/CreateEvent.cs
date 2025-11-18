7b930ca7b797cc66c4f50d4a33a44b076cdeab7e
teddyDWilliams <teddydaviswilliams@gmail.com>
2025-11-17 22:30:08 -0500

    Create Event Functionality v2

--- BlazorApp/CreateEvent.cs
+++ BlazorApp/CreateEvent.cs
@@ -1,13 +1,13 @@
 public class CreateEvent
 {
     //public string? Title { get; set; }
-    //public bool IsDone { get; set; } = false;
-    String title;
-    int day;
-    int month;
-    int year;
-    int startTime;
-    int endTime;
+    public bool IsDone { get; set; } = false;
+    public String title { get; set; }
+    public int day { get; set; }
+    public int month { get; set; }
+    public int year { get; set; }
+    public int startTime { get; set; }
+    public int endTime { get; set; }
     public CreateEvent(String title,int day,int month,int year,int startTime,int endTime)
     {
         this.title = title;
