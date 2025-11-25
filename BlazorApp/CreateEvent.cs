70234984a0b81856f56e19419a223fbd2f03c06b
teddyDWilliams <teddydaviswilliams@gmail.com>
2025-11-25 15:01:34 -0500

    Cleaned up EnterEvent, Display to user month name, tested keyboard input, create usage of DateTime

--- BlazorApp/CreateEvent.cs
+++ BlazorApp/CreateEvent.cs
@@ -8,6 +8,43 @@ public class CreateEvent
     public int year { get; set; }
     public int startTime { get; set; }
     public int endTime { get; set; }
+    public String m
+    {
+        get
+        {
+            
+            switch(month)
+            {
+                case 1:
+                    return "January";
+                case 2:
+                    return "February";
+                case 3:
+                    return "March";
+                case 4:
+                    return "April";
+                case 5:
+                    return "May";
+                case 6: 
+                    return "June";
+                case 7:
+                    return "July";
+                case 8:
+                    return "August";
+                case 9:
+                    return "September";
+                case 10:
+                    return "October";
+                case 11:
+                    return "November";
+                case 12: 
+                    return "December";
+                default:
+                    return "Unknown";
+            }
+        }  
+    }
+    public DateTime date;
     public CreateEvent(String title,int day,int month,int year,int startTime,int endTime)
     {
         this.title = title;
@@ -16,6 +53,7 @@ public class CreateEvent
         this.year = year;
         this.startTime = startTime;
         this.endTime = endTime;
+        
+        date = new DateTime(year,month,day,0,0,0);
     }
-    
 }
